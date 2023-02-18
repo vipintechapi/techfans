0a5597ce9fa5971e5933de36ef25931a231f45da
Vipin Das <118341086+vipinsatlok@users.noreply.github.com>
2023-02-18 17:04:07 +0000

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
+const cors = require("cors")
+
+app.use(cors({ origin: "*" || ["*"] || [""] }))
 
 app.use(express.json())
 
